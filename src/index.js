ed0e3b4d3ceb4417f94c1cedde599e03c700495f
romaindupont <rd.romaindupont@free.fr>
2021-05-24 18:51:34 +0200

    essai

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './styles/main.scss';
-import { menu, menu1 } from './js/menu.js';
+import { menu, menu1, triangle } from './js/menu.js';
 
 const heading = document.createElement('h1')
 heading.textContent = 'Romain Dupont Webpack Config'
 
 // Append heading node to the DOM
 const app = document.querySelector('#root')
-app.append(heading, menu, menu1())
+app.append(heading, menu, menu1(), triangle())
--- src/js/menu.js
+++ src/js/menu.js
@@ -1,8 +1,24 @@
 export const menu = document.createElement('div').textContent ='Import de fichiers OK'
 
 export const menu1 = () => {
-  const nav = document.createElement('div')
-  nav.textContent='le menu'
+  const nav = document.createElement('nav')
+  const ul = document.createElement('ul').textContent="Accueil";
+  const ul1 = document.createElement('ul').textContent="Contact";
+  nav.append(ul, ul1)
   return nav;
 }
 
+export const triangle = () => {
+  const div = document.createElement('div')
+  div.className="logo-menu"
+  const div1 = document.createElement('div')
+  div1.className="triangle1";
+/*   const div2 = div1.cloneNode(true);
+  div1.appendChild(div2) */
+  const div3 = document.createElement('div')
+  div3.className="triangle2"
+/*   const div4 = div3.cloneNode(true)
+  div3.appendChild(div4) */
+  div.append(div1,div3)
+  return div
+}
